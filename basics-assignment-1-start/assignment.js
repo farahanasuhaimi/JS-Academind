869e35aa9370aa8297afa1fdf272b5764093d636
farahanasuhaimi <farahanams@gmail.com>
2021-01-24 11:04:31 +0800

    added first assignment

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 18;
+let result;
+
+result = 18 + userInput;
+
+result = result - userInput;
+result = result * userInput;
+result = result / userInput;
+
+alert(userInput);
+alert(result);
